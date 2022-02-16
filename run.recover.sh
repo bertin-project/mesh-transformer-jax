16ec49e45dd344af4354303a3a69f148acea0963
Javier de la Rosa <versae@gmail.com>
2022-02-16 12:15:59 +0000

    Add recover crashed run

--- run.recover.sh
+++ run.recover.sh
@@ -1,1 +1,1 @@
-python device_train.py --config=configs/ncc_6b_roto_256.recover_81k.json --tune-model-path=gs://nb-gpt-j-6b/mesh_jax_pile_6B_rotary/step_81001
+python device_train.py --config=configs/ncc_6b_roto_256.recover_81k.json --tune-model-path=gs://nb-gpt-j-6b/mesh_jax_pile_6B_rotary/step_81001/
