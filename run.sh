783d2a1a330381dfc8d318b5dbcf512bbc7491f3
Javier de la Rosa <versae@gmail.com>
2022-02-16 16:25:13 +0000

    new run

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python device_train.py --config=configs/ncc_6b_roto_256.json --tune-model-path=gs://nb-gpt-j-6b/step_383500/
+WANDB_ENTITY=NbAiLab python device_train.py --config=configs/ncc_6b_roto_256.json --tune-model-path=gs://nb-gpt-j-6b/step_383500/
