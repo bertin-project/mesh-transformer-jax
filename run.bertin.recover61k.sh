ecea6335abe716e69ed130f1767da69b1fab7af2
Javier de la Rosa <versae@gmail.com>
2022-05-02 09:29:44 +0000

    Add BERTIN configs

---
+++ run.bertin.recover61k.sh
@@ -0,0 +1,1 @@
+WANDB_ENTITY=versae python device_train.py --config=configs/bertin_6b_roto_256_recover61k.json --tune-model-path=gs://bertin-project/gpt-j-6b/step_61001/
---
+++ run.bertin.sh
@@ -0,0 +1,1 @@
+WANDB_ENTITY=versae python device_train.py --config=configs/bertin_6b_roto_256.json --tune-model-path=gs://bertin-project/step_383500/
